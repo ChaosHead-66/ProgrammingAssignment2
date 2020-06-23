41235162e41b30d4516705384bcbb12a9ee4ce05
butt <butt@butt.com>
2020-06-23 17:27:21 +0500

    Assignment 2 - Lexical Scoping - Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -5,16 +5,24 @@
 
 makeCacheMatrix <- function(x = matrix)
 {
-  inv <- NULL
-  set <- function(y)
+  inv <- NULL  #set an empty inverse matrix
+  set <- function(y)  #assign the value of the matrix to x in the parent environment
   {
     x <<- y
-    inv <<- NULL
+    inv <<- NULL   #set an empty inverse matrix in the parent environment
   }
+
+  #gets the value of the matrix
   get <- function() x
+
+
+  #set the value of the inverse matrix in the parent environment 
   setInv <- function(inverse) inv <<- inverse
+
+  #get the value of the inverse matrix
   getInv <- function() inv
   
+  # the following list of clouser functions is returned
   list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
@@ -23,16 +31,23 @@ makeCacheMatrix <- function(x = matrix)
 
 cacheSolve <- function(x, ...) 
 {
+  #gets the value of the inverse matrix
   inv <- x$getInv()
+
+  # if inverse already exists the its cached value is returned
   if(!is.null(inv))
   {
     message('getting cached data')
     return(inv)
   }
   
+  # otherwise , gets the value if matrix
   data <- x$get()
+  # and its inverse is calculated
   inv <- solve(data, ...)
+  # value of inverse is set/cached
   x$setInv(inv) 
   
+  #returns the inverse matrix
   inv
 }
